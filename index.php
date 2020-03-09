174a9ee0c069364694707c50eab88bdae2bb7586
Koji <koji.52884da@gmail.com>
2020-03-08 21:34:46 -0700

    I added propaty and function

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php 
 
 class Menu{
+  public $name;
 
+  public function hello(){
+    echo 'Im '.$this->name;
+  }
 }
 
 $toyota86 = new Menu();
 
 $lexusLc500 = new Menu();
+
+
+$toyota86->name = 'toyota86';
+
+$lexusLc500->name = 'lexusLc500';
+
+
+echo $toyota86->hello();
+echo '<br>';
+echo $lexusLc500->hello();
+
 
 ?>
 
